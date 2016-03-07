4a1539260e68ae5c1af3f17f3cf5e2e6d05c4da9
keenanamigo <keenan.johnson@rideamigos.com>
2016-03-07 05:04:53 +0000

    Added new locations to .txt files, added validation for user input, and spacing to output

--- script.py
+++ script.py
@@ -4,13 +4,25 @@ import random
 domestic = "./destinations/us-destinations.txt" #stores the domestic locations
 international = "./destinations/intl-destinations.txt" #stores the international locations
 
-travel = input("Would you like to travel domestically or internationally for your next adventure? Please enter 'domestic' or 'international': ")
-        
+#Get Input
+travel = raw_input("Would you like to travel domestically or internationally for your next adventure? Please enter 'domestic' or 'international': ")
+travel = travel.lower()
+
+#Validate Input
+while travel not in {'domestic', 'international'}:
+    print("\n")
+    print ("Invalid input. Please enter 'domestic' or 'international' to continue.")
+    print("\n")
+    travel = raw_input("Would you like to travel domestically or internationally for your next adventure? Please enter 'domestic' or 'international': ")
+    travel = travel.lower()
+
+#Get Domestic Locations        
 if (travel == 'domestic'):
     file_obj = open(domestic,'r')
     random_domestic = random.choice(file_obj.readlines())
     file_obj.close()
     print ("Your next adventure should be to: " + random_domestic)
+#Get International Locations    
 elif (travel == 'international'):
     file_obj = open(international,'r')
     random_intl = random.choice(file_obj.readlines())
